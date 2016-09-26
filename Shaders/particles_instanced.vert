2ed343f50158b6d86a7b14f585d227656447e32b
Kevin Moran <kevinmoran007@gmail.com>
2016-09-26 15:39:54 +1000

    Instanced arrays working Quads are now rendering using offsets passed as attributes to the GPU (configured as per-instance instead of the usual per-vertex) This will now scale way better (there's an enforced limit on number of uniforms passed to a shader. Number of attributes is limited only by memory!) and it's way easier to draw more quads; just change the NUM_INSTANCES macro (shaders don't care how many instances are being passed. Awesome!) Next up is to  apply this to a particle system!

--- Shaders/particles_instanced.vert
+++ Shaders/particles_instanced.vert
@@ -1,13 +1,11 @@
 #version 410
 
 layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 offset;
 
 out vec4 quad_color;
 
-uniform vec2 offsets[100];
-
 void main(){
-    vec2 offset = offsets[gl_InstanceID];
     gl_Position = vec4(position + offset, 0.0, 1.0);
     quad_color = vec4(offset.y, offset.x, 0.0, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -26,32 +26,45 @@ int main() {
 		 0.05f,  0.05f
 	};
 
+	//Generate instance offsets
+	#define NUM_INSTANCES 100
+	vec2 instance_offsets[NUM_INSTANCES];
+	srand(time(0));
+	for(int i=0; i<NUM_INSTANCES; i++){
+		instance_offsets[i] = vec2(rand_betweenf(-1,1), rand_betweenf(-1,1));
+	}
+
 	GLuint particle_vao;
+
+	//Vertex points vbo
 	GLuint particle_points_vbo;
 	glGenBuffers(1, &particle_points_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, particle_points_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(particle_points), particle_points, GL_STATIC_DRAW);
 
+	//Instance offsets vbo
+	GLuint instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(instance_offsets), &instance_offsets[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	//Generate VAO
 	glGenVertexArrays(1, &particle_vao);
+	//Bind vertex points vbo
 	glBindVertexArray(particle_vao);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, particle_points_vbo);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	//Bind instance offsets vbo
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+	glVertexAttribDivisor(1, 1); //2nd arg = 1 means updated attribute per instance, not per vertex
 
 	//Load shader
 	Shader particle_shader("particles_instanced.vert", "pass.frag");
 	glUseProgram(particle_shader.prog_id);
-
-	vec2 instance_offsets[100];
-	srand(time(0));
-
-	for(int i=0; i<100; i++){
-		instance_offsets[i] = vec2(rand_betweenf(-1,1), rand_betweenf(-1,1));
-		char offset_string[32];
-		sprintf(offset_string, "offsets[%i]", i);
-		GLint location = glGetUniformLocation(particle_shader.prog_id, offset_string);
-		glUniform2fv(location, 1, instance_offsets[i].v);
-	}
 
 	double curr_time = glfwGetTime(), prev_time, dt;
 	//-------------------------------------------------------------------------------------//
@@ -87,7 +100,7 @@ int main() {
 		//Draw instanced particles
 		glUseProgram(particle_shader.prog_id);
 		glBindVertexArray(particle_vao);
-		glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100);
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 6, NUM_INSTANCES);
 
 		glfwSwapBuffers(window);
 
